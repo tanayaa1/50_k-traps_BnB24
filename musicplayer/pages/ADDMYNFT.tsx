b997af71b8feeb4fc7a00e9cfd3184457650d819
rohanfatehchandka <rvfatehchandka_b21@ce.vjti.ac.in>
2024-02-18 00:57:21 +0530

    Contracts added here

--- musicplayer/pages/ADDMYNFT.tsx
+++ musicplayer/pages/ADDMYNFT.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ADDMYNFT = () => {
   return <div>ADDMYNFT</div>;
-  //Will add Blockchain here
+  //Will add Blockchain here contracts done
 };
 
 export default ADDMYNFT;
